9d3f67e7436a725044630677d2b5f785b6c2dd3b
Felix Breuer <f.breuer94@gmail.com>
2019-12-16 18:47:10 +0100

    Add basic Layout for Tab Navigation

---
+++ src/core/layout/tab-navigator.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from 'react';
+import { Tab } from 'semantic-ui-react';
+
+import Schedule from 'schedule';
+
+function Pane(children): () => ReactElement {
+	return (): ReactElement => (
+		<Tab.Pane style={{ height: '93%' }} attached='top'>
+			{children}
+		</Tab.Pane>
+	);
+}
+
+const panes = [
+	{
+		menuItem: { key: 'schedule', content: 'Schedule', icon: 'clock' },
+		render: Pane(<Schedule />),
+	},
+	{
+		menuItem: { key: 'courses', content: 'Courses', icon: 'rocket' },
+		render: Pane('courses'),
+	},
+	{
+		menuItem: { key: 'settings', content: 'Settings', icon: 'settings' },
+		render: Pane('settings'),
+	},
+];
+
+const TabNavigator = (): ReactElement => (
+	<Tab
+		menu={{ attached: 'bottom', fluid: true, widths: panes.length, style: { height: '7%' } }}
+		style={{ height: '100vh' }}
+		panes={panes}
+	/>
+);
+
+export default TabNavigator;
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,14 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	BrowserRouter as Router,
+} from 'react-router-dom';
 
-import Navbar from './navbar';
-
-const sleep = (ms): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
-
-async function test(): Promise<void> {
-	await sleep(3000);
-	console.log('hello after 3 seconds');
-}
-
-function App(): ReactElement {
-	return (
-		<div>
-			<Navbar title='hallo' />
-			<p>hello wold</p>
-			<button type='button' onClick={test}>click me</button>
-		</div>
-	);
-}
+import TabNavigator from 'core/layout/tab-navigator';
 
 ReactDOM.render(
-	<App />
+	<Router>
+		<TabNavigator />
+	</Router>
 	, document.getElementById('app')
 );
